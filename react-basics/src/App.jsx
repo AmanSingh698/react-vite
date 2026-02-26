138c39fd09cbe5381b9b27980c82aa080be3849a
Aman Singh <aman.singh@msllearning.com>
2026-02-26 11:51:50 +0530

    react basics

---
+++ react-basics/src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div id="papa">
+        <h1 id="child1">Child 1</h1>
+        <h2 id="child2">Child 2</h2>
+        <h3 id="child3">Child 3</h3>
+      </div>
+
+      <div id="chahcha">
+        <h1 id="grandchild1">Grandchild 1</h1>
+        <h2 id="grandchild2">Grandchild 2</h2>
+        <h3 id="grandchild3">Grandchild 3</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ script.js
@@ -0,0 +1,5 @@
+// var h1 = document.createElement("h1");
+// // console.log(h1);
+
+// h1.innerHTML = "Hello from Aman";
+// document.body.appendChild(h1);
